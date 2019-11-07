35c6ed12cb0a43ce10c482e8ac9a6605ed2bd5ab
Wiebe Kremer <wiebe.kremer96@gmail.com>
2019-11-07 16:00:21 +0100

    Added function to clear data and sum average
    splitStringCalcAverage(data) splits and calls average function
    
    clearDateString(data) clears data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,159 @@
 import results from './data/results.json'
 // import cleanData from './lib/cleanYears'
 
-var fetchedData = results.results.bindings
-console.log(fetchedData)
+const rawData = results.results.bindings
+
+function main() {
+	convertYear(rawData);
+}
+
+console.log(splitStringCalcAverage(results.results.bindings[10].date.value))
+
+function splitStringCalcAverage(data) {
+    var splittedString = data.split("-")
+    return average(splittedString[0], splittedString[1])
+}
+
+function average(a, b) {
+    // force the input as numbers *1
+    return Math.round((a*1 + b*1) /2);
+}
+
+function clearDateString(data) {
+    data.value = data.value.toUpperCase();
+
+
+    if(data.value && typeof data.value === "string") {
+        // Remove Spaces
+        data.value = data.value.replace(/\s+/g, '');
+
+        if (data.value.includes("BC")) {
+            data.bcValue = true
+            data.value = data.value.replace('BC', "");
+            if (data.value.includes("AD")) {
+                data.adValue = true
+                data.value = data.value.replace('AD', "");
+            } else {
+                data.adValue = false
+            }
+        } else {
+            data.bcValue = false
+        }
+
+        if (data.value.includes("EEUW")) {
+            data.eeuw = true
+            data.value = data.value.replace('EEUW', "");
+
+            if (data.value.includes("VROEG")) {
+                data.value = data.value.replace('VROEG', "");
+
+                if (data.value.includes("E")) {
+                    data.value = data.value.replace(/E/g, "");
+                }
+            } else if (data.value.includes("MIDDEN")) {
+                data.value = data.value.replace('MIDDEN', "");
+                if (data.value.includes("E")) {
+                    data.value = data.value.replace(/E/g, "");
+                }
+            } else if (data.value.includes("E")) {
+                data.value = data.value.replace(/E/g, "");
+            }
+
+        } else if (data.value.includes("TH")) {
+            data.value = data.value.replace(/\T.*/,'');
+            data.eeuw = true
+        } else {
+            data.eeuw = false
+        }
+
+        if (data.value.includes("CA")) {
+            data.value = data.value.replace('CA.', "");
+        }
+        if (data.value.includes("NA")) {
+            data.value = data.value.replace('NA', "");
+        }
+        if (data.value.includes("C.")) {
+            data.value = data.value.replace('C.', "");
+        }
+        if (data.value.includes("D")) {
+            data.value = data.value.replace('D', "");
+        }
+        if (data.value.includes("CIRCA")) {
+            data.value = data.value.replace('CIRCA', "");
+        }
+        if (data.value.includes("(VOOR")) {
+            data.value = data.value.replace('(VOOR', "");
+        }
+        if (data.value.includes("VOOR")) {
+            data.value = data.value.replace(/VOOR/g, "");
+        }
+
+        if (data.value.includes("ST")) {
+            data.value = data.value.replace('ST', "");
+        }
+
+
+        if (data.value.includes("(")) {
+            data.value = data.value.replace(/ *\([^)]*\) */g, '');
+        } else if (data.value.includes(")")) {
+            data.value = data.value.replace(")", '');
+        }
+
+
+        if (data.value.includes("-")) {
+            data.value = data.value.replace('CIRCA', "");
+        }
+        if (data.value.includes("/")) {
+            data.value = data.value.replace('/', "-");
+        }
+
+        if (data.value.includes("?")) {
+            data.value = data.value.replace('?', "");
+        }
+        if (data.value.includes("20-20THCNTURY")){
+            data.value = data.value.replace('20-20THCNTURY', "20");
+            data.eeuw = "0"
+        }
+        if (data.value.includes("SCHAAL") || data.value.includes("2HLFT19")) {
+            data.value = "0"
+        }
+    }
+
+    return data
+}
+
+
+function convertYear(item) {
+    // item = clearDateString(item)
+    // var type = checkType(item)
+    //
+    // if (type === "simple"){
+    //     return averageSimpleYear(item)
+    // }
+	item.map(el => {
+
+        var clearedStrings = clearDateString(el.date)
+
+        // console.log(clearedStrings)
+
+        if (clearedStrings.value.includes("-")) {
+            if (clearedStrings.eeuw == false){
+                clearedStrings.value = splitStringCalcAverage(clearedStrings.value);
+                console.log(clearedStrings.value)
+            }
+        }
+
+
+
+        // Heb ik average nodig, check ik hier
+
+	})
+
+	let newArr = item;
+	return newArr;
+}
+
+main();
 
 // fetchedData.forEach(function(i){
 //     i.img.value = i.img.value.replace("http", "https")
